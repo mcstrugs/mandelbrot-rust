221bb42d44a015b116a04421f1a79cde6a652e4a
Joe Martinson <joe.martinson@modalai.com>
2021-04-29 15:52:23 -0700

    implement bool set, printing of set

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use num_complex::Complex64;
+use array2d::Array2D;
 
 fn main() {
-    asciiset(1000,500,1000);
+    let set = bool_set(200,100,1000);
+    print_set(set);
 }
 
+#[allow(dead_code)]
 fn asciiset(w: u32,h: u32,count: u32) {
     let diff_x: f64 = 4f64 / w as f64;
     let diff_y: f64 = 4f64 / h as f64;
@@ -18,6 +21,36 @@ fn asciiset(w: u32,h: u32,count: u32) {
         }
         print!("\n");
     }
+}
+
+#[allow(dead_code)]
+fn print_set(set: Array2D<bool>) {
+    for row in set.rows_iter() {
+        for element in row {
+            if *element {
+                print!("#");
+            } else {
+                print!(" ");
+            }
+        }
+        print!("\n");
+    }
+}
+
+#[allow(dead_code)]
+fn bool_set(w: u32, h: u32, count: u32) -> Array2D<bool> {
+    let mut set = Array2D::filled_with(false, h as usize,w as usize);
+    let diff_x: f64 = 4f64 / w as f64;
+    let diff_y: f64 = 4f64 / h as f64;
+    for i in 0..h {
+        for j in 0..w {
+            let comp = Complex64::new(-2.0 + diff_x * j as f64,2.0 - diff_y * i as f64);
+            if in_set(comp, count) {
+                let res = set.set(i as usize,j as usize,true);
+            }
+        }
+    }
+    set
 }
 
 fn in_set(c: Complex64, iter_count: u32) -> bool {
